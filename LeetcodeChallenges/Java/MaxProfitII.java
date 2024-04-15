e3ad2fa044147c2d6b9be92709b525b8ebc2c7ec
Yash <yashisrani35@gmail.com>
2024-04-15 16:26:06 +0200

    Length of last word

---
+++ LeetcodeChallenges/Java/MaxProfitII.java
@@ -0,0 +1,15 @@
+class MaxProfitII {
+    /*
+     * @param gg
+     */
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        MaxProfitII profit = new MaxProfitII();
+        int maxProfit;
+        int[] prices = { 6, 4, 2, 1 };
+        maxProfit = profit.maxProfit(prices);
+    }
+}
