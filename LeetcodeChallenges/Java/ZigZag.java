1cc99c7816df9577a0b6ab388c9790178e6de33b
Yash <yashisrani35@gmail.com>
2024-07-18 16:38:32 +0200

    ZigZag

---
+++ LeetcodeChallenges/Java/ZigZag.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class ZigZag {
+	// Had no idea how to do this needed to look at solution
+	public String convert(String s, int numRows) {
+		if (numRows == 1 || numRows >= s.length()) {
+			return s;
+		}
+
+		int idx = 0, d = 1;
+		List<Character>[] rows = new ArrayList[numRows];
+		for (int i = 0; i < numRows; i++) {
+			rows[i] = new ArrayList<>();
+		}
+
+		for (char c : s.toCharArray()) {
+			rows[idx].add(c);
+			if (idx == 0) {
+				d = 1;
+			} else if (idx == numRows - 1) {
+				d = -1;
+			}
+			idx += d;
+		}
+
+		StringBuilder result = new StringBuilder();
+		for (List<Character> row : rows) {
+			for (char c : row) {
+				result.append(c);
+			}
+		}
+
+		return result.toString();        
+	}
+	
+
+	public static void main(String[] args) {
+		ZigZag objZag = new ZigZag();
+		String returnedString = objZag.convert("PAYPALISHIRING", 3);
+		System.out.print(returnedString);
+	}
+}
